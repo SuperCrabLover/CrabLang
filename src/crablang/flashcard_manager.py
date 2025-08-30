66a7462a22aa0e3f77ecb423de71e6d5e2770407
SuperCrabLover <molozhavenko.aa@phystech.edu>
2025-08-30 16:51:10 +0300

    simple flashcard implementation

--- src/crablang/flashcard_manager.py
+++ src/crablang/flashcard_manager.py
@@ -10,25 +10,31 @@ import chardet
 
 class Flashcard:
     def __init__(self, term: str, definition: str):
-        pass
+        self._term = term
+        self._definition = definition
+        self._correct_guesses = 0
+        self._incorrect_guesses = 0
+
+    def is_correct_guess(self, definition) -> bool:
+        return self._definition == definition
 
     def correct_guess(self) -> None:
-        pass
+        self._correct_guesses += 1
 
     def incorrect_guess(self) -> None:
-        pass
+        self._incorrect_guesses += 1
 
     @property
     def get_correct_guesses_number(self) -> int:
-        pass
+        return self._correct_guesses
 
     @property
     def get_incorrect_guesses_number(self) -> int:
-        pass
+        return self._incorrect_guesses
 
     @property
     def get_guesses_number(self) -> int:
-        pass
+        return self._incorrect_guesses + self._correct_guesses
 
 
 class FlashcardManager:
